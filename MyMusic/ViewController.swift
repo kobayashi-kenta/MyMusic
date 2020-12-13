e25ba487c6c0ff7af5bf6a0387447e259bc4c5ca
kobayashi-kenta <12321klj@gmail.com>
2020-12-13 14:02:13 +0900

    Initial Commit

---
+++ MyMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMusic
+//
+//  Created by Kenta on 2020/12/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
