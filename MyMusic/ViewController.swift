9aae041a9e11c92f09e7ab06ab1e54bf39b1c773
kobayashi-kenta <12321klj@gmail.com>
2020-12-13 23:23:04 +0900

    音源機能の実装

--- MyMusic/ViewController.swift
+++ MyMusic/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -13,7 +14,64 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    //シンバルmp３ファイル指定
+    let cymbalPath = Bundle.main.bundleURL.appendingPathComponent("cymbal.mp3")
+    
+    //シンバル用プレイヤーインスタンスの追加
+    var cymbalPlayer = AVAudioPlayer()
 
+    @IBAction func cymbal(_ sender: Any) {
+        do {
+        cymbalPlayer = try AVAudioPlayer(contentsOf: cymbalPath, fileTypeHint: nil)
+        
+        cymbalPlayer.play()
+        } catch {
+            print("シンバルでエラーが発生しました！")
+        }
+    }
+    
+    
+    //ギターmp3ファイル指定
+    let guitarPath = Bundle.main.bundleURL.appendingPathComponent("guitar.mp3")
+    //ギター用プレイヤーインスタンスの追加
+    var guitarPlayer = AVAudioPlayer()
+    
+    
+    @IBAction func guitar(_ sender: Any) {
+        do {
+            guitarPlayer = try AVAudioPlayer(contentsOf: guitarPath, fileTypeHint: nil)
+            
+            guitarPlayer.play()
+        } catch {
+            print("ギターでエラーが発生しました！")
+        }
+    }
+    
+    //バックミュージックの音源ファイルを指定
+    let backmusicPath = Bundle.main.bundleURL.appendingPathComponent("backmusic.mp3")
+    //バックミュージック用の音源インスタンスを作成
+    
+    var backmusicPlayer = AVAudioPlayer()
+    
+    @IBAction func play(_ sender: Any) {
+        do {
+            //バックミュージック用のプレイヤーに音源ファイルを指定
+            backmusicPlayer = try AVAudioPlayer(contentsOf: backmusicPath,fileTypeHint: nil)
+            
+            //リピート設定
+            backmusicPlayer.numberOfLoops = -1
+            backmusicPlayer.play()
+        } catch {
+            print("エラーが発生しました！")
+        }
+    }
+    
+    //Stopボタンの処理
+    
+    @IBAction func stop(_ sender: Any) {
+         backmusicPlayer.stop()
 
+    }
 }
 
